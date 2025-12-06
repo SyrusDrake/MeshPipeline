86064c74305570eaf2d70d820aea09561b1f06bc
Florian Frühwirth <flo.fruehwirth@gmail.com>
2025-12-06 03:38:56 +0100

    CSV output
    Including csv output, now essentially feature-complete. Saving before trying a different approach with itertools.

--- poisson.py
+++ poisson.py
@@ -1,4 +1,6 @@
 import pymeshlab
+from csv import writer
+from os import stat
 
 ms = pymeshlab.MeshSet()
 
@@ -7,18 +9,19 @@ input_mesh = 'test_cloud'
 depth: int = 8  # Default is 8, max recommended is 12
 pointweight: float = 4.0  # Default is 4.0
 preclean: bool = False  # Default is False
+mesh_statistics = {}
 
 
 ms.load_new_mesh(f"{input_mesh}.ply")
 print("Mesh loaded.")
 
 # Iterate over a range of depth and pointweight values
-for i in range(10, 11):
+for i in range(9, 10):
     # Depths are increased in steps of 1
 
     depth = i
 
-    for j in range(10, 16, 5):
+    for j in range(0, 16, 5):
         """
         IMPORTANT!
         Point weights are increaseed in steps of 0.5, however, since floats cannot be used in the file name, they are multiplied by 10.
@@ -34,7 +37,40 @@ for i in range(10, 11):
         print(
             f"Surface reconstruction completed for depth {depth} and point weight {pointweight}.")
 
+        ms.compute_scalar_by_distance_from_another_mesh_per_vertex(
+            measuremesh=ms.current_mesh_id(), refmesh=0)
+        print("Distance computation completed.")
+
         # Save the reconstructed mesh with parameters in the filename. Note that the vertex quality is not calculated as of now, the function will be added later.
+        meshname = f"d{depth}_pw{(j)}_pc{preclean}"
+
         ms.save_current_mesh(
-            f"output/{input_mesh}(d{depth}_pw{j}_pc{preclean}).ply", binary=False, save_vertex_quality=True)
+            f"output/{input_mesh}({meshname}).ply", binary=False, save_vertex_quality=True)
         print("Reconstructed mesh saved.")
+
+        filesize = (stat(f"output/{input_mesh}({meshname}).ply").st_size)/1024
+
+        mesh_statistics[meshname] = ms.get_scalar_statistics_per_vertex()
+        mesh_statistics[meshname]['d'] = depth
+        mesh_statistics[meshname]['p'] = pointweight
+        mesh_statistics[meshname]['c'] = str(preclean).lower()
+        mesh_statistics[meshname]['size_kb'] = filesize
+
+with open('data.csv', 'w', newline='') as file:
+    writer = writer(file)
+    writer.writerow(['Mesh',
+                     'Average', 'Max', 'Median', 'Min', 'StdDev', 'Variance', 'Depth', 'Pointweight', 'Preclean', 'Size_KB'])
+    for key, value in mesh_statistics.items():
+        writer.writerow([key,
+                         value['avg'],
+                         value['max'],
+                         value['med'],
+                         value['min'],
+                         value['stddev'],
+                         value['variance'],
+                         value['d'],
+                         value['p'],
+                         value['c'],
+                         value['size_kb']])
+
+print('done')
