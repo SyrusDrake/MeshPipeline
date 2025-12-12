c16e16997baf10231a1fa324ae37b5724f53bd89
Florian Frühwirth <flo.fruehwirth@gmail.com>
2025-12-12 21:58:50 +0100

    Complete with GUI
    GUI works as intended, output as expected

---
+++ app.py
@@ -0,0 +1,73 @@
+import gradio as gr
+from poisson import mesh_analysis
+
+depth_range = range(0, 13)  # Default is 8, max recommended is 12
+# Default is 4.0, no idea what the recommended max is, arbritrarily set to 8.0
+pointweight_range = range(0, 81, 5)
+preclean: bool = False  # Default is False
+mesh_statistics = {}
+input_file: str
+
+
+def upload_file(filepath):
+    # Required to make upload button work.
+    return filepath
+
+
+def gui():
+    """Draws the GUI for the mesh analyzer.
+
+    Returns:
+        None
+    """
+
+    with gr.Blocks() as comparison:
+
+        gr.Markdown("# Mesh Analyzer")  # : Title of the app
+
+        #: input mesh
+        selected_file = gr.File(label="Input Mesh (.ply)", file_types=[
+                                ".ply"], interactive=True)
+
+        with gr.Row():
+            #: depth range
+            depth_range_slider_min = gr.Slider(label="min depth", minimum=depth_range.start, maximum=depth_range.stop -
+                                               1, step=1, interactive=True)
+            depth_range_slider_max = gr.Slider(label="max depth", minimum=depth_range.start, maximum=depth_range.stop -
+                                               1, step=1, interactive=True)
+        with gr.Row():
+            #: pointweight range
+
+            pointweight_range_slider_min = gr.Slider(
+                label="min pointweight", minimum=pointweight_range.start/10, maximum=(pointweight_range.stop-1)/10, step=0.5, interactive=True)
+            pointweight_range_slider_max = gr.Slider(
+                label="max pointweight", minimum=pointweight_range.start/10, maximum=(pointweight_range.stop-1)/10, step=0.5, interactive=True)
+
+        #: save mesh option
+        save_mesh_checkbox = gr.Checkbox(
+            label="Save Reconstructed Meshes", value=True, interactive=True)
+
+        #: create csv option
+        create_csv_checkbox = gr.Checkbox(
+            label="Create CSV with Statistics", value=True, interactive=True)
+
+        #: output folder
+        output_folder_selector = gr.Textbox(
+            label="Output Folder", placeholder="Enter output folder path", interactive=True, value="./output/")
+
+        #: csv filename
+        csv_filename_textbox = gr.Textbox(
+            label="CSV Filename", placeholder="Enter CSV filename", interactive=True, value="mesh_statistics.csv")
+
+        #: run button
+        run_button = gr.Button("Run Mesh Analysis")
+        run_button.click(fn=mesh_analysis,
+                         inputs=[selected_file, depth_range_slider_min, depth_range_slider_max, pointweight_range_slider_min,
+                                 pointweight_range_slider_max, output_folder_selector, save_mesh_checkbox, create_csv_checkbox, csv_filename_textbox],
+                         outputs=[])
+
+    comparison.launch(inbrowser=True)
+
+
+if __name__ == "__main__":
+    gui()
--- poisson.py
+++ poisson.py
@@ -1,69 +1,121 @@
 import pymeshlab
-from csv import writer
-from os import stat
+from csv import writer as writer
+from os import stat, path
 import itertools
 
-ms = pymeshlab.MeshSet()
+"""The following variables are not used in practices, but remain included for testing, if necessary."""
 
-input_mesh = 'test_cloud'
+input_mesh: str  # Path to input mesh without extension
 
 depth: int  # Default is 8, max recommended is 12
 depth_range = range(4, 6)
 pointweight: float  # Default is 4.0
 pointweight_range = range(0, 41, 5)  # From 0.0 to 4.0 in steps of 0.5
 preclean: bool = False  # Default is False
-mesh_statistics = {}
 
 
-ms.load_new_mesh(f"{input_mesh}.ply")
-print("Mesh loaded.")
+def mesh_analysis(input_mesh_path: str, depth_min: int, depth_max: int, pointweight_min: int, pointweight_max: int, output_folder: str = "./output", save_mesh: bool = True, create_csv: bool = True, csv_filename: str = "data.csv"):
+    """
+    Performs a Poisson surface reconstruction on the input mesh over a range of depth and pointweight values, computes distance to reference as vertex quality, saves the resulting meshes, and generates a CSV file with statistics.
 
-# Iterate over a range of depth and pointweight values
-for pointweight, depth, preclean in itertools.product(pointweight_range, depth_range, [False, True]):
+    :param input_mesh_path: The complete file path to the input mesh.
+    :type input_mesh_path: str
 
-    ms.set_current_mesh(
-        0)  # Reset to the original import mesh before each reconstruction, otherwise, the previous reconstruction will be used as the input for the next one.
+    :param depth_min: Minimum depth value for Poisson reconstruction.
+    :type depth_min: int
+    :param depth_max: Maximum depth value for Poisson reconstruction.
+    :type depth_max: int
 
-    ms.generate_surface_reconstruction_screened_poisson(depth=depth,
-                                                        pointweight=pointweight/10,
-                                                        preclean=preclean)
-    print(
-        f"Surface reconstruction completed for depth {depth} and point weight {pointweight/10}.")
+    :param pointweight_min: Minimum pointweight value for Poisson reconstruction. Note that the input is divided by 10 for the actual reconstruction (and is thus a float), but is required as an integer for the file name.
+    :type pointweight_min: int
+    :param pointweight_max: Maximum pointweight value for Poisson reconstruction.
+    :type pointweight_max: int
 
-    ms.compute_scalar_by_distance_from_another_mesh_per_vertex(
-        measuremesh=ms.current_mesh_id(), refmesh=0)
-    print("Distance computation completed.")
+    :param output_folder: Folder where output files will be saved. Default is "./output".
+    :type output_folder: str
 
-    # Save the reconstructed mesh with parameters in the filename.
-    meshname = f"d{depth}_pw{(pointweight)}_pc{preclean}"
+    :param save_mesh: Whether to save the reconstructed meshes. Default is True.
+    :type save_mesh: bool
 
-    ms.save_current_mesh(
-        f"output/{input_mesh}({meshname}).ply", binary=False, save_vertex_quality=True)
-    print("Reconstructed mesh saved.")
+    :param create_csv: Whether to create a CSV file with mesh statistics. Default is True.
+    :type create_csv: bool
 
-    filesize = (stat(f"output/{input_mesh}({meshname}).ply").st_size)/1024
+    :param csv_filename: Name of the CSV file to be created. Default is "data.csv".
+    :type csv_filename: str
+    """
 
-    mesh_statistics[meshname] = ms.get_scalar_statistics_per_vertex()
-    mesh_statistics[meshname]['d'] = depth
-    mesh_statistics[meshname]['p'] = pointweight
-    mesh_statistics[meshname]['c'] = str(preclean).lower()
-    mesh_statistics[meshname]['size_kb'] = filesize
+    mesh_statistics = {}  # Dictionary to hold statistics for each mesh
+    # Turning the min and max values into a range
+    depth_range = range(depth_min, depth_max + 1)
+    pointweight_range = range(int(pointweight_min*10),
+                              int((pointweight_max*10)+1), 5)  # Turning the min and max values into a range
+    # Get the name of the input mesh without file path and extension
+    input_mesh_name = path.basename(input_mesh_path)[:-4]
 
-with open('data.csv', 'w', newline='') as file:
-    writer = writer(file)
-    writer.writerow(['Mesh',
-                     'Average', 'Max', 'Median', 'Min', 'StdDev', 'Variance', 'Depth', 'Pointweight', 'Preclean', 'Size_KB'])
-    for key, value in mesh_statistics.items():
-        writer.writerow([key,
-                         value['avg'],
-                         value['max'],
-                         value['med'],
-                         value['min'],
-                         value['stddev'],
-                         value['variance'],
-                         value['d'],
-                         value['p'],
-                         value['c'],
-                         value['size_kb']])
+    # This keeps throwing an error, I don't know why, but it still works.
+    ms = pymeshlab.MeshSet()
 
-print('done')
+    ms.load_new_mesh(input_mesh_path)
+    print("Mesh loaded.")
+
+    # Iterate over a range of depth and pointweight values
+    for pointweight, depth, preclean in itertools.product(pointweight_range, depth_range, [False, True]):
+
+        ms.set_current_mesh(
+            0)  # Reset to the original import mesh before each reconstruction, otherwise, the previous reconstruction will be used as the input for the next one.
+
+        ms.generate_surface_reconstruction_screened_poisson(depth=depth,
+                                                            pointweight=pointweight/10,
+                                                            preclean=preclean)  # Perform Poisson surface reconstruction
+        print(
+            f"Surface reconstruction completed for depth {depth} and point weight {pointweight/10}.")
+
+        ms.compute_scalar_by_distance_from_another_mesh_per_vertex(
+            measuremesh=ms.current_mesh_id(), refmesh=0)  # Compute distance to the input cloud as vertex quality
+        print("Distance computation completed.")
+
+        # Create a name for the mesh based on current parameters
+        meshname = f"d{depth}_pw{(pointweight)}_pc{preclean}"
+
+        if save_mesh:
+            # Save the reconstructed mesh with parameters in the filename.
+            ms.save_current_mesh(
+                f"{output_folder}/{input_mesh_name}({meshname}).ply", binary=False, save_vertex_quality=True)  # Binary false to save as human readable ASCII, save vertex quality to include the calculated distances
+            print("Reconstructed mesh saved.")
+
+            filesize = (
+                stat(f"{output_folder}/{input_mesh_name}({meshname}).ply").st_size)/1024  # Get file size in KB
+        else:
+            # If not saving the mesh, set filesize to 0, so that something can be recorded in the CSV.
+            filesize = 0
+
+        # Get statistics of the vertex quality (distances)
+        mesh_statistics[meshname] = ms.get_scalar_statistics_per_vertex()
+        mesh_statistics[meshname]['d'] = depth
+        # Store pointweight as the actually used float
+        mesh_statistics[meshname]['p'] = pointweight/10
+        mesh_statistics[meshname]['c'] = str(preclean).lower()
+        mesh_statistics[meshname]['size_kb'] = filesize
+        mesh_statistics[meshname]['vertices'] = ms.current_mesh(
+        ).face_number()  # Store number of faces
+
+    if create_csv:
+        # Store dict into a CSV file
+        with open(f"{output_folder}/{csv_filename}", 'w', newline='') as file:
+            file_writer = writer(file)
+            file_writer.writerow(['Mesh',
+                                  'Average', 'Max', 'Median', 'Min', 'StdDev', 'Variance', 'Depth', 'Pointweight', 'Preclean', 'Size_KB', 'Faces'])
+            for key, value in mesh_statistics.items():
+                file_writer.writerow([key,
+                                      value['avg'],
+                                      value['max'],
+                                      value['med'],
+                                      value['min'],
+                                      value['stddev'],
+                                      value['variance'],
+                                      value['d'],
+                                      value['p'],
+                                      value['c'],
+                                      value['size_kb'],
+                                      value['faces']])
+    print('done')
